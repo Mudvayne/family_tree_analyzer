918894ce31c7b550bfa6f033bb00d23d85a7a662
Manuel <stigaccs@mail.com>
2014-08-14 18:19:34 +0200

    fix for heroku issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-#FamilyTreeAnalyzer::Application.config.session_store :active_record_store
-FamilyTreeAnalyzer::Application.config.session_store :cache_store
+FamilyTreeAnalyzer::Application.config.session_store :active_record_store
+#FamilyTreeAnalyzer::Application.config.session_store :cache_store
